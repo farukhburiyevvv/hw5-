cce560ef35fc50a2ee3eb2e93ffe993f88716d1f
farrukh <f.buriyev@newuu.uz>
2024-11-04 16:35:28 +0500

    Create problem5.cpp

---
+++ problem5.cpp
@@ -0,0 +1,33 @@
+
+#include <iostream>
+using namespace std;
+#include <bits/stdc++.h>
+
+
+int main()
+{
+    string s,big,small;
+    int b,sm;
+    getline(cin,s);
+    cout<<endl;
+    
+    for(int i=0;i<s.size();i++){
+        if(isupper(s[i])){
+            b++;
+            big=big+" "+s[i];
+        } 
+        else{
+            small=small+" "+s[i];
+            sm++;
+        }
+         
+    }
+    cout<<b<<big<<endl;
+    cout<<sm<<small;
+    
+       
+    
+           
+        
+    return 0;
+}
